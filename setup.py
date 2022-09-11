a66c3055a0ff3278714f1eaf861b87db239c3f51
nishi7409 <nishi7409@gmail.com>
2022-09-10 22:43:12 -0400

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ropy1',
+      version='1.0',
+      description='A Python package to utilize when interacting with ROBLOX\'s API',
+      url='http://github.com/nishi7409/RoPy',
+      author='Nishant Srivastava',
+      author_email='ropy@rsrivastava.io',
+      license='MIT',
+      packages=['ropy1'],
+      zip_safe=False)
